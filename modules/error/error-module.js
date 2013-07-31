af32826e7803ba3ef25c74a2e3d50dd7f4c3edfb
Steven Holms <superlinkx@gmail.com>
2013-07-31 02:20:34 -0700

    Added error module framework
    Allows implementations to set the log level, and automatically logs to the console (using log, warn, error),
    and logs to an internal array, which can be easily manipulated later (for dumping to file or uploading to server)

--- modules/error/error-module.js
+++ modules/error/error-module.js
@@ -4,30 +4,59 @@ define(config.substructure_paths("error"), function() {
 	//Declare module namespace
 	error.module_name = "error";
 	error.messages = [];
-	error.log = function(msg, type, extra) {
+	error.loglevel = 4; //loglevels are 0 - 4, crit through debug
+	error.log = function(type, msg, extra) {
 		//msg is required. Contains the message you wish to display.
 		//type is optional. Defaults to a debug message type.
 		//extra is optional. Is an object. When implemented, will contain additional logging information that isn't meant to be part of the actual message.
-		switch(type) {
-			case "debug":
-				//debug
-				break;
-			case "info":
-				//info
-				break;
-			case "warn":
-				//warnings
-				break;
-			case "err":
-				//Error
-				break;
-			case "crit":
-				//critical Error
-				break;
-			default:
-				//debug
-		}
+
+		if(this.loglevel >= 0)
+			if(type == "crit") {
+				console.error("CRITICAL: "+msg);
+				if(extra)
+					console.error(extra);
+				this.pushMsg(type, msg, extra);
+			}
+
+		if(this.loglevel >= 1)
+			if(type == "err") {
+				console.error("ERROR: "+msg);
+				if(extra)
+					console.error(extra);
+				this.pushMsg(type, msg, extra);
+			}
+
+		if(this.loglevel >= 2)
+			if(type == "warn") {
+				console.warn("WARNING: "+msg);
+				if(extra)
+					console.warn(extra);
+				this.pushMsg(type, msg, extra);
+			}
+
+		if(this.loglevel >= 3)
+			if(type == "info") {
+				console.log("INFO: "+msg);
+				if(extra)
+					console.log(extra);
+				this.pushMsg(type, msg, extra);
+			}
+			
+		if(this.loglevel >= 4)
+			if(type == "debug") {
+				console.log("DEBUG: "+msg);
+				if(extra)
+					console.log(extra);
+				this.pushMsg(type, msg, extra);
+			}
 	};
+
+	error.pushMsg = function(type, msg, extra) {
+		if(extra)
+			this.messages.push({"type": type, "message": msg, "extra": extra});
+		else
+			this.messages.push({"type": type, "message": msg});
+	}
 
 	return error; //Return module
 });
