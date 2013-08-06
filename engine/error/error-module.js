a08ab98f3e1816d12d99e157526780681df0a274
Steven Holms <superlinkx@gmail.com>
2013-08-06 02:54:40 -0700

    Updated error module to be more efficient.
    Now only listens to error publications that correspond to the current loglevel
    
    Added loglevel changing method to help facilitate this. Updates error.loglevel, and publishes
    the new log level. A new loglevelChyanged subscription then unsubscribes current error
    subscriptions, and resubscribes to publications that meet the new loglevel.
    
    TODO: Needs error checking to make sure that the new loglevel is valid

--- engine/error/error-module.js
+++ engine/error/error-module.js
@@ -4,6 +4,11 @@ define([], function() {
 	
 	error.messages = [];
 	error.loglevel = 4; //loglevels are 0 - 4, crit through debug
+	error.postCrit = [];
+	error.postErr = [];
+	error.postWarn = [];
+	error.postInfo = [];
+	error.postDbg = [];
 	
 	//Error posting methods
 	//Receive a data object
@@ -12,73 +17,82 @@ define([], function() {
 	//data.extra - Optional object. Contains arbitrary data in object notation.
 
 	error.crit = function(data) {
-		if(this.loglevel >= 0) {
-			console.error("CRITICAL: "+data.msg);
-			if(data.extra)
-				console.error(data.extra);
-			this.messages.push(data);
-			//Engine shutdown
-			$.publish("haltEngine");
-		}
+		data.type = "crit";
+		console.error("CRITICAL: "+data.msg);
+		if(data.extra)
+			console.error(data.extra);
+		this.messages.push(data);
+		//Engine shutdown
+		$.publish("haltEngine");
 	};
 
 	error.err = function(data) {
-		if(this.loglevel >= 1) {
-			console.error("ERROR: "+data.msg);
-			if(data.extra)
-				console.error(data.extra);
-			this.messages.push(data);
-		}
+		data.type = "err";
+		console.error("ERROR: "+data.msg);
+		if(data.extra)
+			console.error(data.extra);
+		this.messages.push(data);
 	};
 
-	error.warn = function(data) { 
-		if(this.loglevel >= 2) {
-			console.warn("WARNING: "+data.msg);
-			if(data.extra)
-				console.warn(data.extra);
-			this.messages.push(data);
-		}
+	error.warn = function(data) {
+		data.type = "warn";
+		console.warn("WARNING: "+data.msg);
+		if(data.extra)
+			console.warn(data.extra);
+		this.messages.push(data);
 	};
 
-	error.info = function(data) { 
-		if(this.loglevel >= 3) {
-			console.log("INFO: "+data.msg);
-			if(data.extra)
-				console.log(data.extra);
-			this.messages.push(data);
-		}
+	error.info = function(data) {
+		data.type = "info";
+		console.log("INFO: "+data.msg);
+		if(data.extra)
+			console.log(data.extra);
+		this.messages.push(data);
 	};
 
 	error.dbg = function(data) {
-		if(this.loglevel >= 4) {
-			console.log("DEBUG: "+data.msg);
-			if(data.extra)
-				console.log(data.extra);
-			this.messages.push(data);
+		data.type = "dbg";
+		console.log("DEBUG: "+data.msg);
+		if(data.extra)
+			console.log(data.extra);
+		this.messages.push(data);
+	};
+
+	error.changeLogLevel = function(loglevel) {
+		error.loglevel = loglevel;
+		$.publish("loglevelChanged", [loglevel]);
+	};
+
+	error.updateLoglevelSub = function(loglevel) {
+		$.unsubscribe(this.postDbg);
+		$.unsubscribe(this.postInfo);
+		$.unsubscribe(this.postWarn);
+		$.unsubscribe(this.postErr);
+		$.unsubscribe(this.postCrit);
+		switch(loglevel) {
+			case 4:
+				this.postDbg = $.subscribe("postDbg", function(data) {error.dbg(data);});
+			case 3:
+				this.postInfo = $.subscribe("postInfo", function(data) {error.info(data);});
+			case 2:
+				this.postWarn = $.subscribe("postWarn", function(data) {error.warn(data);});
+			case 1:
+				this.postErr = $.subscribe("postErr", function(data) {error.err(data);});
+			case 0:
+				this.postCrit = $.subscribe("postCrit", function(data){error.crit(data);});
+				break;
+			default:
+				return -1;
+				break;
 		}
 	};
 
 	//Subscribe to listeners
-	$.subscribe("postDbg", function(data) {
-		data.type = "dbg";
-		error.dbg(data);
+	$.subscribe("loglevelChanged", function(loglevel) {
+		error.updateLoglevelSub(loglevel);
 	});
-	$.subscribe("postInfo", function(data) {
-		data.type = "info";
-		error.info(data);
-	});
-	$.subscribe("postWarn", function(data) {
-		data.type="warn";
-		error.warn(data);
-	});
-	$.subscribe("postErr", function(data) {
-		data.type="err";
-		error.err(data);
-	});
-	$.subscribe("postCrit", function(data) {
-		data.type="crit";
-		error.crit(data);
-	});
+	//Setup default loglevel
+	error.changeLogLevel(4);
 
 	return error; //Return module
 });
